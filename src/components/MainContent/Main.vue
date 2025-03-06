019a70c59462cbf6abc17dd7393965a1d38db986
cendyz <arek57823@gmail.com>
2025-03-06 21:02:12 +0100

    SecondSection mobile

--- src/components/MainContent/Main.vue
+++ src/components/MainContent/Main.vue
@@ -1,11 +1,13 @@
 <template>
-	<main class="px-[3rem]">
+	<main class="px-[3rem] grid gap-y-[7rem]">
 		<FirstSection />
+		<SecondSection />
 	</main>
 </template>
 
 <script setup lang="ts">
 import FirstSection from './FirstSection.vue'
+import SecondSection from './SecondSection.vue';
 </script>
 
 <style scoped></style>
--- src/components/MainContent/SecondSection.vue
+++ src/components/MainContent/SecondSection.vue
@@ -1,21 +1,93 @@
 <template>
-	<section>
-		<div>
-			<div>
-				<p>page views</p>
-				<img src="" alt="facebook" />
+	<section class="grid gap-y-[2rem]">
+		<h2 class="text-[2.5rem] font-w700 pb-[2.2rem]">Overview - Today</h2>
+		<div v-for="(item, index) in data" :key="index" class="p-[2.5rem] bg-light-cardBg rounded-xl grid gap-y-[2.8rem]">
+			<div class="flex items-center justify-between">
+				<p class="font-w700 text-light-text">{{ item.title }}</p>
+				<img :src="item.img" alt="facebook" class="w-[2.2rem]" />
 			</div>
-			<div>
-				<p>82</p>
-				<div>
-					<img src="" alt="up" />
-					<p>3%</p>
+			<div class="flex items-center justify-between">
+				<p class="font-w700 text-[4rem] text-light-darkText leading-[1]">{{ item.num }}</p>
+				<div class="flex items-center gap-x-[.5rem]">
+					<img :src="item.arrow" alt="up" class="w-[1rem]" />
+					<p class="font-w700" :class="item.arrow === up ? 'text-primary-limeGreen' : 'text-primary-brightRed'">
+						{{ item.procent }}%
+					</p>
 				</div>
 			</div>
 		</div>
 	</section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import facebook from '@/images/icon-facebook.svg'
+import instagram from '@/images/icon-instagram.svg'
+import twitter from '@/images/icon-twitter.svg'
+import youtube from '@/images/icon-youtube.svg'
+import up from '@/images/icon-up.svg'
+import down from '@/images/icon-down.svg'
+
+const data = reactive({
+	0: {
+		title: 'Page Views',
+		img: facebook,
+		arrow: up,
+		num: 87,
+		procent: 3,
+	},
+	1: {
+		title: 'Likes',
+		img: facebook,
+		arrow: down,
+		num: 52,
+		procent: 2,
+	},
+	2: {
+		title: 'Likes',
+		img: instagram,
+		arrow: up,
+		num: 5462,
+		procent: 2257,
+	},
+	3: {
+		title: 'Profile Views',
+		img: instagram,
+		arrow: up,
+		num: '52k',
+		procent: 1375,
+	},
+	4: {
+		title: 'Retweets',
+		img: twitter,
+		arrow: up,
+		num: 117,
+		procent: 303,
+	},
+	5: {
+		title: 'Likes',
+		img: twitter,
+		arrow: up,
+		num: 507,
+		procent: 553,
+	},
+	6: {
+		title: 'Likes',
+		img: youtube,
+		arrow: down,
+		num: 107,
+		procent: 19,
+	},
+	7: {
+		title: 'Total views',
+		img: youtube,
+		arrow: down,
+		num: 1407,
+		procent: 12,
+	},
+})
+
+console.log(data[0].arrow === up)
+</script>
 
 <style scoped></style>
